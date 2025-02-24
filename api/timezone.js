962a608781859c942aa65f8855e1362caf468a15
HyperRushNet <hyperrushnetwork@gmail.com>
2025-02-24 21:54:00 +0100

    Update timezone.js

--- api/timezone.js
+++ api/timezone.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-        const response = await fetch("https://ipapi.co/json");
+        const response = await fetch("http://edns.ip-api.com/json");
         const data = await response.json();
 
         res.status(200).json(data);
