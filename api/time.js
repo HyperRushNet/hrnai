5cfc55cf9077b2c2840e124e4549fa451186dfbf
HyperRushNet <hyperrushnetwork@gmail.com>
2025-02-24 21:34:14 +0100

    Update time.js

--- api/time.js
+++ api/time.js
@@ -1,17 +1,21 @@
 export default function handler(req, res) {
-    // CORS header toevoegen
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Toegestaan voor alle domeinen
-    res.setHeader('Access-Control-Allow-Methods', 'GET'); // Alleen GET-methodes toestaan
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Toegestane headers
-
-    // De tijd berekenen en terugsturen
     const date = new Date();
+
+    // Converteer naar CET (UTC+1) en houd rekening met zomertijd
     const options = { 
+        timeZone: 'Europe/Amsterdam', // CET en CEST afhankelijk van de tijd van het jaar
         hour: '2-digit', 
         minute: '2-digit', 
         second: '2-digit', 
         timeZoneName: 'short' 
     };
+
     const timeString = date.toLocaleTimeString('nl-NL', options);
+
+    // Voeg CORS headers toe
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); 
+
     res.status(200).json({ time: timeString });
 }
