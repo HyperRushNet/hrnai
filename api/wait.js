ffd3be4c36c3c460df190f8d8f73edf6f29d51f1
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-06 00:38:42 +0200

    Delete api/wait.js

--- api/wait.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  // Wacht 20 seconden
-  await new Promise(resolve => setTimeout(resolve, 30000));
-  
-  // Geef een antwoord terug na 20 seconden
-  res.status(200).json({ message: "Wachten voltooid, 60 seconden gewacht!" });
-}
