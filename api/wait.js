e2e0298ea748390f5220185b44c0c5a85bdebe3b
HyperRushNet <hyperrushnetwork@gmail.com>
2025-04-06 00:28:10 +0200

    Update wait.js

--- api/wait.js
+++ api/wait.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   // Wacht 20 seconden
-  await new Promise(resolve => setTimeout(resolve, 61000));
+  await new Promise(resolve => setTimeout(resolve, 60000));
   
   // Geef een antwoord terug na 20 seconden
-  res.status(200).json({ message: "Wachten voltooid, 61 seconden gewacht!" });
+  res.status(200).json({ message: "Wachten voltooid, 60 seconden gewacht!" });
 }
